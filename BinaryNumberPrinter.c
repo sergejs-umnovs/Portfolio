b4055a1a2c10093b01b97b3783f1f91f81899cb1
Sergejs Umnovs <71380665+sergejs-umnovs@users.noreply.github.com>
2020-09-21 10:35:40 -0700

    Update BinaryNumberPrinter.c

--- BinaryNumberPrinter.c
+++ BinaryNumberPrinter.c
@@ -2,23 +2,23 @@
 #include <conio.h>
 
 // 
-// ѕрограмма, котора€ в цикле выводит числа в диапазоне unsigned int в двоичном формате
+// Program, which prints numbers in binary in a for loop
 // 
 
-void printBin(unsigned int number) { // ничего не возвращаем, потому что эта функци€ нужна толька дл€ вывода
-	unsigned int temp = number >> 24; // переменна€ хранени€ резултата сдвига. получаем значение старшего байта переменной number
+void printBin(unsigned int number) { // don't need to return anything
+	unsigned int temp = number >> 24; // a variable to store binary shift result. Get the most significant byte value of the function argument
 	printf("%d%d%d%d%d%d%d%d ", (temp & 128) == 128, (temp & 64) == 64, (temp & 32) == 32, (temp & 16) == 16, (temp & 8) == 8, (temp & 4) == 4, (temp & 2) == 2, temp & 1);
-	temp = number >> 16; // получаем значение второго байта слева переменной number
+	temp = number >> 16; // get second most significant byte of the function argument
 	printf("%d%d%d%d%d%d%d%d ", (temp & 128) == 128, (temp & 64) == 64, (temp & 32) == 32, (temp & 16) == 16, (temp & 8) == 8, (temp & 4) == 4, (temp & 2) == 2, temp & 1);
-	temp = number >> 8; // получаем значение третьего байта слева переменной number
+	temp = number >> 8; // get third most significant byte of the function argument
 	printf("%d%d%d%d%d%d%d%d ", (temp & 128) == 128, (temp & 64) == 64, (temp & 32) == 32, (temp & 16) == 16, (temp & 8) == 8, (temp & 4) == 4, (temp & 2) == 2, temp & 1);
-	temp = number; // получаем значение младшего байта переменной number
+	temp = number; // get least significant byte of the function argument
 	printf("%d%d%d%d%d%d%d%d\n", (temp & 128) == 128, (temp & 64) == 64, (temp & 32) == 32, (temp & 16) == 16, (temp & 8) == 8, (temp & 4) == 4, (temp & 2) == 2, temp & 1);
-} // выводные конструкции работают только с младшим байтом переменной
+} // printing functions work with least significant byte of the variable
 
 int main() {
-	for (int i = 0; i <= 65536; ++i) // в цикле выводим значени€ i с помощью нашей функции
+	for (int i = 0; i <= 65536; ++i) // print i value in binary via the function in a for loop
 		printBin(i);
-	getch(); // останавливаемс€, чтобы посмотреть результат
+	getch(); // pause the program to see the result
 	return 0;
 }

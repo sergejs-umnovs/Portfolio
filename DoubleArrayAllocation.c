65e55b99f5e5069689b0d7a7168d9de06fffa5c0
Sergejs Umnovs <71380665+sergejs-umnovs@users.noreply.github.com>
2020-09-21 11:03:18 -0700

    Update DoubleArrayAllocation.c

--- DoubleArrayAllocation.c
+++ DoubleArrayAllocation.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define arrayLength 4 // опр. длину массива
-#define arrayWidth 7 // опр. ширину массива
+#define arrayLength 4 // define array length
+#define arrayWidth 7 // define array width
 
 // 
-// Программа для изучения двумерных динамических массивов 
+// Programs, which explains dynamically allocated two-dimensional arrays
 // 
 
 int main() {
 	
 	int *pt = (int *) malloc(arrayLength * arrayWidth * sizeof(int)); 
-	//после   резервирования, надо отформатировать память под нужный тип данных, резервируем в данном случае 112 байт
-	int i, j, c; // объявляем переменные
-	srand(time(0)); // добавляем сид для рандома
-	for (i = 0; i < arrayWidth; i++) // идём по ширине массива
-		for (j = 0; j < arrayLength; j++) //идём по длине массива
-			*(pt + i*arrayWidth + j)= rand()%100 + 1; //задаём рандомные значения от 0 до 99, но в итоге у нас будет от 1 до 100
+	//must explicitly typecast0, in this case, we allocate 112 bytes
+	int i, j, c; // declare variables
+	srand(time(0)); // set the random number seed
+	for (i = 0; i < arrayWidth; i++) 
+		for (j = 0; j < arrayLength; j++) 
+			*(pt + i*arrayWidth + j)= rand()%100 + 1; // fill the two-dimensional array with data
 
 	for (i = 0; i < arrayWidth; i++) {
 		for (j = 0; j < arrayLength; j++)
-			printf("%d\n", *(pt + i*arrayWidth + j)); //выводим всё, что у нас есть
+			printf("%d\n", *(pt + i*arrayWidth + j)); //print everything in the array
 		printf("\n");  
     }
-	free(pt);
+	free(pt); // free the memory
 	return 0;
 }

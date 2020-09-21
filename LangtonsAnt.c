168026fb10f9424e740f7e1a615f9d98f1d94a51
Sergejs Umnovs <71380665+sergejs-umnovs@users.noreply.github.com>
2020-09-21 11:30:45 -0700

    Update LangtonsAnt.c

--- LangtonsAnt.c
+++ LangtonsAnt.c
@@ -11,12 +11,12 @@
 #define PI 3.1415
 
 //
-// Программа-симуляция муравья Лэнгтона (YouTube в курсе), только в консоли
-// Выдаёт 256 сценариев поведения
+// Simulation of the Langton's ant, but in the console
+// Generates 256 scenarios of ant's behaviour
 //
 
-char screen[SCRH][SCRW] = {}; //экранный буфер
-typedef struct { //структура муравья
+char screen[SCRH][SCRW] = {}; // screen buffer 
+typedef struct { // ant's struct
     void (*think)();
     void (*move)();
     void (*turn)();
@@ -25,11 +25,11 @@ typedef struct { //структура муравья
     int angle;
     } Ant, *Aptr;
     
-void moveAnt(Aptr); //прототипы функций
+void moveAnt(Aptr); // function prototypes
 void thinkAnt(Aptr);
 void turnAnt(Aptr, int);
 
-struct Dirs { //битовое поле контроля направлений
+struct Dirs { // behaviour control bit field
 	int bit1:1;
 	int bit2:1;
 	int bit3:1;
@@ -41,14 +41,14 @@ struct Dirs { //битовое поле контроля направлений
 } directionControl;
 
 int main() {
-	Ant ant = {thinkAnt,moveAnt,turnAnt,{10, 40},{-1, 0}, 90}; // инициализируем муравь
-	for(int D = 0; D < 256; D++) { // цикл, который задаёт сценарии
-		for(int i=0;i<SCRH;i++) { // чистим экран
+	Ant ant = {thinkAnt,moveAnt,turnAnt,{10, 40},{-1, 0}, 90}; // initialize the ant
+	for(int D = 0; D < 256; D++) { // behaviour scenario selecting loop
+		for(int i=0;i<SCRH;i++) { // clear the screen
 			for(int j=0;j<SCRW;j++) {
 				screen[i][j] = '%';
 				}
 			}
-		ant.pos[0] = 10; // возвращаем муравья в центр
+		ant.pos[0] = 10; // move the ant to the middle of the screen
 		ant.pos[1] = 40;
 		
 		ant.movVec[0] = -1;
@@ -56,19 +56,19 @@ int main() {
 		
 		ant.angle = 90;
 		
-		while((ant.pos[0] <= 20 && ant.pos[0] >= 0) && (ant.pos[1] <= 80 && ant.pos[1] >= 0)){ // следя за его позицией, заставляем его думать и передвигаться
+		while((ant.pos[0] <= 20 && ant.pos[0] >= 0) && (ant.pos[1] <= 80 && ant.pos[1] >= 0)){ // make it think, terminate if it goes out of screen bounds
 			ant.think(&ant);
 			ant.move(&ant);
 			//Sleep(100);
 		}
 		for(int i=0;i<SCRH;i++) {
 			for(int j=0;j<SCRW;j++) {
-				printf("%c",screen[i][j]); //выводим экранный буфер
+				printf("%c",screen[i][j]); //print screen buffer
 				}
 				printf("\n");
 			}
 		printf("\n");
-		directionControl.bit1=D&(1<<0)?1:0; //модифицируем битовое поле
+		directionControl.bit1=D&(1<<0)?1:0; //modify bit field
 		directionControl.bit2=D&(1<<1)?1:0;
 		directionControl.bit3=D&(1<<2)?1:0;
 		directionControl.bit4=D&(1<<3)?1:0;
@@ -77,21 +77,21 @@ int main() {
 		directionControl.bit7=D&(1<<6)?1:0;
 		directionControl.bit8=D&(1<<7)?1:0;
 	}
-	getch(); // ставим на паузу
+	getch(); // pause the program
 	return 0;
 }
     
-void moveAnt(Aptr target) { //функция передвижения
-    target->pos[0] += target->movVec[0]; //вычисляем с помошью векторной суммы
+void moveAnt(Aptr target) { // movement function
+    target->pos[0] += target->movVec[0]; // compute position using vectors
     target->pos[1] += target->movVec[1];
     }
     
-void turnAnt(Aptr target, int direction) { // функция поворота, используется в thinkAnt
+void turnAnt(Aptr target, int direction) { // rotation fucntion
     target->angle += 90*direction;
-    target->movVec[0] = round(sinf(target->angle*PI/180)); // преобразует поворот в направление. быстрое решение, но ресурсозатратное
+    target->movVec[0] = round(sinf(target->angle*PI/180)); // makes movement vectors. a fast, but resource-demanding solution
     target->movVec[1] = round(cosf(target->angle*PI/180));
     }
-void thinkAnt(Aptr target) { // контроль поведения муравья
+void thinkAnt(Aptr target) { // ant control
         int y = target->pos[0],x = target->pos[1];
         switch (screen[y][x]) {
 		case '%':

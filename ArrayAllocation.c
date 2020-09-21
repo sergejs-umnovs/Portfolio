a2564dbec86df794cd6b3ea518db4053e6fc2258
Sergejs Umnovs <71380665+sergejs-umnovs@users.noreply.github.com>
2020-09-21 10:24:35 -0700

    Update ArrayAllocation.c

--- ArrayAllocation.c
+++ ArrayAllocation.c
@@ -8,23 +8,23 @@
 //
 
 int main() {
-	int array[SIZE] = {}; //					- генерируем статический массив
-	int *array2 = (int*)malloc(SIZE * sizeof(int)); //		- генерируем динамический массив
-	printf("size of normal array %d\n",sizeof(array)); //		- выводим размер статического массива
-	printf("size of generated array %d\n\n",sizeof(array2));//	- выводим размер указателя на выделенную память
-	for(int i = 0; i < SIZE; i++) { //				- заполняем статический массив и динамический массив
+	int array[SIZE] = {}; //					- declare and initialize an array
+	int *array2 = (int*)malloc(SIZE * sizeof(int)); //		- dynamically allocate array
+	printf("size of normal array %d\n",sizeof(array)); //		- print first array length
+	printf("size of generated array %d\n\n",sizeof(array2));//	- print dynamically allocated pointer size
+	for(int i = 0; i < SIZE; i++) { //				- fill both arrays
 		array[i] = i;
 		*(array2+i) = 2*i;
 	}
 	printf("Contents of normal array:\n");
-	for(int i = 0; i < SIZE; i++) { //				- выводим данные статического массива, в данном случае должен выдать: 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+	for(int i = 0; i < SIZE; i++) { //				- print first array data, must print: 1 2 3 4 5 6 7 8 9 10 11 12 13
 		printf("%d ",array[i]);
 	}
 	printf("\nContents of generated array:\n"); 
-	for(int i = 0; i < SIZE; i++) { //				- выводим данные динамического массива, в данном случае должен выдать: 0 2 4 6 8 10 12 14 16 18 20 22 24 26
+	for(int i = 0; i < SIZE; i++) { //				- print dynamically allocated array data, must print: 0 2 4 6 8 10 12 14 16 18 20 22 24 26
 		printf("%d ",*(array2+i));
 	}
-	free(array2); //						- освобождаем выделенную память
+	free(array2); //						- deallocate dynamically allocated array
 	
 	return 0;
 }
